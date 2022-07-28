d724e7abbb1156d543c7ccf62dfe8bd569b063de
heejun-yoo <heejunyoo@tosspayments.com>
2022-07-28 22:52:30 +0900

    update billing auth (rubyonrails)

--- billing-auth-api/rubyonrails/config/routes.rb
+++ billing-auth-api/rubyonrails/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
     # add code
-    get 'billing/index' => 'billing#index'
-    get 'billing/billing_confirm' => 'billing#billing_confirm'
-    get 'billing/fail' => 'billing#fail'
+    get 'billing/billing_auth' => 'billing#billing_auth'
+    
     # end code
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
